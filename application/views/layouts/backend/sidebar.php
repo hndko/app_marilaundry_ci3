dd0d4f42a69921257835120656f9d7bde6f0e459
hndko <hd.doko22@gmail.com>
2026-02-16 13:00:31 +0700

    feat: Add backend sidebar layout including navigation, user panel, and content wrapper.

--- application/views/layouts/backend/sidebar.php
+++ application/views/layouts/backend/sidebar.php
@@ -9,7 +9,7 @@
 		<!-- Sidebar user (optional) -->
 		<div class="user-panel mt-3 pb-3 mb-3 d-flex">
 			<div class="image">
-				<img src="<?= base_url('assets/dist/img/user2-160x160.jpg') ?>" class="img-circle elevation-2" alt="User Image">
+				<img src="<?= base_url('assets/dist/img/avatar.png') ?>" class="img-circle elevation-2" alt="User Image">
 			</div>
 			<div class="info">
 				<a href="<?= site_url('profile') ?>" class="d-block"><?= current_user()->fullname ?? current_user()->username ?></a>
